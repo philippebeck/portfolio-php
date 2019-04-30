d0af65a100aed3b699ec1dbee550c4a135320bd1
Philippe Beck <philippe@philippebeck.net>
2019-04-30 13:03:03 +0200

    Add the index.php with the FrontController init

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+use Pam\Controller\FrontController;
+
+
+// Loads Composer autoload
+require_once dirname(__DIR__).'/vendor/autoload.php';
+
+
+// Creates a front controller instance
+$frontController = new FrontController();
+
+
+// Basic tests area
+// print_r($_SESSION);
+// var_dump($frontController);
+
+// Calls the run method on the front controller instance
+$frontController->run();
