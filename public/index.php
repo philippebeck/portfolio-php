ba17c861b5dd6dad0264024199a5a2aab6fe1828
Philippe Beck <philippe@philippebeck.net>
2019-05-01 17:28:47 +0200

    Update the index with Tracy

--- public/index.php
+++ public/index.php
@@ -1,18 +1,20 @@
 <?php
 
+// ***************** \\
+// ***** INDEX ***** \\
+// ***************** \\
 
 use Pam\Controller\FrontController;
-
+use Tracy\Debugger;
 
 // Loads Composer autoload
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
-
 // Creates a front controller instance
 $frontController = new FrontController();
 
-
 // Basic tests area
+Debugger::enable();
 // print_r($_SESSION);
 // var_dump($frontController);
 
