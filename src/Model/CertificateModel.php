1bb3ce4be8d2aab86ad8cad9809c3b7d2cc60792
Philippe Beck <philippe@philippebeck.net>
2019-05-03 07:58:33 +0200

    Create all Models

---
+++ src/Model/CertificateModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model;
+
+use Pam\Model\Model;
+
+/** *******************\
+ * CRUD for certificates
+ */
+class CertificateModel extends Model {}
---
+++ src/Model/UserModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model;
+
+use Pam\Model\Model;
+
+/** ***********\
+ * CRUD for user
+ */
+class UserModel extends Model {}
