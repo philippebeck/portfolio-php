8eaa64554837f11e985e9a2f0543a90fc9b99451
Philippe Beck <philippe@philippebeck.net>
2019-05-03 10:23:08 +0200

    Create the UserController

---
+++ src/Controller/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Pam\Controller\Controller;
+use Pam\Helper\Session;
+use Pam\Model\ModelFactory;
+
+/**
+ * Class UserController
+ * @package App\Controller
+ */
+class UserController extends Controller
+{
+    /**
+     * @return string
+     */
+    public function loginAction()
+    {
+        if (!empty($_POST)) {
+            $user = ModelFactory::get('User')->read($_POST['email'], 'email');
+
+            if (password_verify($_POST['pass'], $user['pass'])) {
+                Session::createSession(
+                    $user['id'],
+                    $user['first_name'],
+                    $user['image'],
+                    $user['email']
+                );
+
+                htmlspecialchars(Session::createAlert('Successful authentication, welcome ' . $user['first_name'] .' !', 'purple'));
+
+                $this->redirect('home');
+
+            } else {
+                htmlspecialchars(Session::createAlert('Failed authentication !', 'gray'));
+            }
+        }
+        return $this->render('login.twig');
+    }
+
+    /**
+     *
+     */
+    public function logoutAction()
+    {
+        Session::destroySession();
+
+        $this->redirect('home');
+    }
+}
