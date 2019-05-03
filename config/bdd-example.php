f86b79f9114bddf33bb6cd593c45e5fbe6ee2ad8
Philippe Beck <philippe@philippebeck.net>
2019-05-03 11:33:57 +0200

    Create a bdd-example file

---
+++ config/bdd-example.php
@@ -0,0 +1,15 @@
+<?php
+
+// You need to replace dbhost with the database host
+define('DB_HOST', 'dbhost');
+
+// You need to replace dbname with the database name
+define('DB_NAME', 'dbname');
+
+// You need to replace username with the user name of the database
+define('DB_USER', 'username');
+
+// You need to replace userpass with the user password of the database
+define('DB_PASS', 'userpass');
+
+// Then remove the '-example' from 'bdd-example.php' to get 'bdd.php'
