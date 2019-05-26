21cacbc2c46d10c1ae4e99dc67fd830b2059b8b3
Philippe Beck <philippe@philippebeck.net>
2019-05-26 12:16:42 +0200

    Add comments to index to explain debugger set

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 use Pam\Controller\FrontController;
 use Pam\Helper\Session;
+
+// For Development only (needs to be comment in Production)
 use Tracy\Debugger;
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
@@ -9,7 +11,7 @@ require_once dirname(__DIR__).'/vendor/autoload.php';
 $session            = new Session();
 $frontController    = new FrontController();
 
-// Basic tests area
+// For Development only (needs to be comment in Production)
 Debugger::enable();
 // print_r($_SESSION);
 // var_dump($frontController);
