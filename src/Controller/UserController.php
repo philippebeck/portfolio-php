dcca18d75b85460224f0d6182cc980309d58817d
Philippe Beck <philippe@philippebeck.net>
2019-07-16 21:29:21 +0200

    Encapsulate INPUT_POST

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -22,10 +22,12 @@ class UserController extends Controller
      */
     public function loginAction()
     {
-        if (!empty(filter_input_array(INPUT_POST))) {
-            $user = ModelFactory::get('User')->read(filter_input(INPUT_POST, 'email'), 'email');
+        $post = filter_input_array(INPUT_POST);
 
-            if (password_verify(filter_input(INPUT_POST, 'pass'), $user['pass'])) {
+        if (!empty($post)) {
+            $user = ModelFactory::get('User')->read($post['email'], 'email');
+
+            if (password_verify($post['pass'], $user['pass'])) {
                 $this->session->createSession(
                     $user['id'],
                     $user['name'],
