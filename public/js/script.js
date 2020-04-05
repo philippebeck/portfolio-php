e4b14ccd3f47342f0b1bee2f37ad98233b595752
Philippe Beck <philippe@philippebeck.net>
2020-04-05 18:22:21 +0200

    Add the local script for recaptcha

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function(e) {
-    
-});
+var onloadCallback = function() {
+    grecaptcha.render('recaptcha', {
+        'sitekey' : '6Ld_2eYUAAAAAPHLRqN5mOEqZJqNKq7RO01CcwIy'
+    });
+};
